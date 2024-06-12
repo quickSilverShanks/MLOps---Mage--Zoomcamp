e26ef682216cd4cfa987a802a8720bcb10a3cfc4
Shashank Prakash <31253156+quickSilverShanks@users.noreply.github.com>
2024-06-12 10:05:36 +0000

    added train model block in pipeline

--- mlops/assignment_03/transformers/train_model.py
+++ mlops/assignment_03/transformers/train_model.py
@@ -1,3 +1,5 @@
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LinearRegression
 
 if 'transformer' not in globals():
     from mage_ai.data_preparation.decorators import transformer
@@ -21,8 +23,23 @@ def transform(df, *args, **kwargs):
         Anything (e.g. data frame, dictionary, array, int, str, etc.)
     """
     # Specify your transformation logic here
+    categorical = ['PULocationID', 'DOLocationID']
+    train_dicts = df[categorical].to_dict(orient='records')
 
-    return df
+    dv = DictVectorizer()
+    X_train = dv.fit_transform(train_dicts)
+
+    target = 'duration'
+    y_train = df[target].values
+
+    lr = LinearRegression()
+    lr.fit(X_train, y_train)
+
+    y_pred = lr.predict(X_train)
+
+    print(lr.intercept_)
+
+    return dv, lr
 
 
 @test
